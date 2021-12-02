1a66c0c2b383392e75228133410f42a816079bd8
yo-yan <yooyan0512@gmail.com>
2021-12-02 20:20:57 +0900

    google

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/compat/app';
+import firebase from 'firebase/app';
 import "firebase/firestore";
 import "firebase/auth";
 
@@ -15,9 +15,35 @@ var firebaseConfig = {
     measurementId: process.env.REACT_APP_APP_ID
 };
 
+console.log(firebaseConfig)
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
+
+//googleLogin
+export const provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+firebase.auth().signInWithRedirect(provider);
+
+export const googleIn = async () => {
+    await provider
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
 
 export default firebase;
